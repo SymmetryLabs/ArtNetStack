c5ec79aa73301cd148607bef57ce85aef5230a55
Kyle Fleming <kyle@kylefleming.net>
2018-08-15 21:07:56 -0700

    Accept strings starting with 0x for hexStringToByteArray

--- src/main/java/fr/azelart/artnetstack/utils/ByteUtilsArt.java
+++ src/main/java/fr/azelart/artnetstack/utils/ByteUtilsArt.java
@@ -50,10 +50,15 @@ public final class ByteUtilsArt {
 	
 	public static byte[] hexStringToByteArray(String s) {
 	    int len = s.length();
+	    int offset = 0;
+			if (s.startsWith("0x")) {
+				len -= 2;
+				offset += 2;
+			} 
 	    byte[] data = new byte[len / 2];
 	    for (int i = 0; i < len; i += 2) {
-	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-	                             + Character.digit(s.charAt(i+1), 16));
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(offset+i), 16) << 4)
+	                             + Character.digit(s.charAt(offset+i+1), 16));
 	    }
 	    return data;
 	}
